1aa2193a703c407e5085ed756db4e51af07d2192
Tushar <tushargarg0110@gmail.com>
2022-10-05 19:44:22 +0530

    changes

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -10,19 +10,21 @@ public class Main {
         double ans3 = p.product(1.2,1.4);
         System.out.println(ans+" "+ans1+" "+ans3);
     }
+
+    static class Product{
+
+        public int product(int x, int y) {
+
+            return x*y;
+        }
+        public int product(int x,int y,int z){
+            return x*y*z;
+        }
+        public double product(double x, double y) {
+            return x*y;
+        }
+
+    }
+
 }
 
-class Product{
-
-    public int product(int x, int y) {
-
-        return x*y;
-    }
-    public int product(int x,int y,int z){
-        return x*y*z;
-    }
-    public double product(double x, double y) {
-        return x*y;
-    }
-
-}
