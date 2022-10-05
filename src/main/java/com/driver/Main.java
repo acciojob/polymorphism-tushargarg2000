0758676729d0ccffd996f295b6102a75a4d2064a
Tushar <tushargarg0110@gmail.com>
2022-10-05 19:35:03 +0530

    Added code

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,4 +2,27 @@ package com.driver;
 
 public class Main {
 
+    public static void main(String[] args){
+
+        Product p = new Product();
+        int ans = p.product(1,2);
+        int ans1 = p.product(1,2,3);
+        double ans3 = p.product(1.2,1.4);
+        System.out.println(ans+" "+ans1+" "+ans3);
+    }
+}
+
+class Product{
+
+    public int product(int x, int y) {
+
+        return x*y;
+    }
+    public int product(int x,int y,int z){
+        return x*y*z;
+    }
+    public double product(double x, double y) {
+        return x*y;
+    }
+
 }
