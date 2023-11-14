63b674acb0d9f5db4b007e2c2e3a9fe74cbb3e6f
Tushar <tushargarg0110@gmail.com>
2023-11-14 21:49:49 +0530

    Code changes for products

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -6,20 +6,17 @@ public class Main {
 
         Product p = new Product();
 
-        int ans = p.product(10,20);
+        int ans1 = p.product(10,20);
 
-        int ans1 = p.product(10,20,30);
+        int ans2 = p.product(10,20,30);
 
-        double ans2 = p.product(1.0,2.0);
+        double ans3 = p.product(10.0,20.0);
 
-        System.out.println(ans+" "+ans1+" "+ans2);
+        System.out.println(ans1+" "+ans2+" "+ans3);
 
 
     }
-
-    public static class Product {
-
-
+    static class Product { //I am making a mistake here and not setting it public
 
         public int product(int x, int y) {
             return x*y;
@@ -32,11 +29,6 @@ public class Main {
         public double product(double x, double y) {
             return x*y;
         }
-
-
-
-
     }
 
 }
-
