5d68ff58526ce053a549507f6b1db4f2ad2093df
Tushar <tushargarg0110@gmail.com>
2023-12-12 22:45:21 +0530

    Code changes done for polymorphism

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -6,17 +6,18 @@ public class Main {
 
         Product p = new Product();
 
-        int ans1 = p.product(10,20);
+        int ans = p.product(10,20);
 
-        int ans2 = p.product(10,20,30);
 
-        double ans3 = p.product(10.0,20.0);
+        int ans1 = p.product(10,20,30);
 
-        System.out.println(ans1+" "+ans2+" "+ans3);
+        double ans2 =  p.product(10.0,20.0);
 
+        System.out.println(ans+" "+ans1+" "+ans2);
 
     }
-    public static class Product { //I am making a mistake here and not setting it public
+
+    static class Product {
 
         public int product(int x, int y) {
             return x*y;
