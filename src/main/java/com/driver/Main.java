e55c32cd11972f75c0c438ecea995087a8da4fae
Tushar <tushargarg0110@gmail.com>
2024-04-12 21:42:08 +0530

    Assignment submitted

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -6,18 +6,17 @@ public class Main {
 
         Product p = new Product();
 
+        Person2 p2 = new Person2();
+
         int ans = p.product(10,20);
+        int ans1 = p.product(10,20,304);
 
-        int ans1 = p.product(1,2,3);
+        double ans3 = p.product(1.0,2.0);
 
-        double  ans2 = p.product(1.0,2.0);
-
-        System.out.println(ans+" "+ans1+" "+ans2);
-
-
+        System.out.println(ans+" "+ans1+" "+ans3);
     }
 
-    public static class Product {
+    static class Product {
 
         public int product(int x, int y) {
             return x*y;
@@ -33,4 +32,9 @@ public class Main {
 
     }
 
+}
+
+class Person2{
+
+    int age;
 }
